2cdb539f4fc3c20d9e8f874d1c22add1915635ea
Andrew Hummel <andrew.hummel@inspire11.com>
2019-12-06 10:04:33 -0600

    Updates

--- component---src-pages-404-tsx-f174cb7712c45d7498f9.js
+++ component---src-pages-404-tsx-f174cb7712c45d7498f9.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[3],{"i6+/":function(t,n,e){"use strict";e.r(n);e("q1tI");var o=e("Wbzz"),u=e("NqE+"),c=e("TBFr"),d=e("+ego"),l=e("qKvR");n.default=function(){return Object(l.d)(d.a,null,Object(l.d)(u.a,null,Object(l.d)(c.a,null,Object(l.d)("h1",null,"404: Page not found."),Object(l.d)("p",null,"You've hit the void. ",Object(l.d)(o.a,{to:"/"},"Go back.")))))}}}]);
-//# sourceMappingURL=component---src-pages-404-tsx-6f460b13d64c1f4aedc8.js.map
+//# sourceMappingURL=component---src-pages-404-tsx-f174cb7712c45d7498f9.js.map
